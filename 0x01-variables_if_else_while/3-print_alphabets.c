e7b4543793e30d682f1ff24a803f993cc4a904a1
GWTelvin <telvinwachira75@gmail.com>
2022-09-10 02:04:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
@@ -21,7 +20,7 @@ int main(void)
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 
 	{
-	
+
 		putchar(ch);
 
 	}
@@ -39,4 +38,3 @@ int main(void)
 	return (0);
 
 }
-
