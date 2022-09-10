db53390799093743f7a3ad8ba9f2ce4494a98ff1
GWTelvin <telvinwachira75@gmail.com>
2022-09-10 00:16:40 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,21 @@
  * main -print if the number is positive, zero, or negative
  *
  * Description: using the main function 
+ *
  * this program prints "Programming is positive, zero, or negative"
- * Return: 0
+ *
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
 
 	putchar('\n');
+
 	return (0);
 }
