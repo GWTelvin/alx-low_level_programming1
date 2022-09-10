4de111407ff849bcd35710ab1be66a19101bdae7
GWTelvin <telvinwachira75@gmail.com>
2022-09-10 02:14:50 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - printing numbers from 0-9 with commas and space between them
+ * main - printing numbers from 0-9 with commas and space between them
  *
  * Description: using the main function
  *
@@ -16,26 +15,25 @@ int main(void)
 
 {
 
-		int c;
+	int c;
 
+		for (c = 48; c <= 57; c++)
 
-			for (c = 48; c <= 57; c++)
+			{
 
-				{
+				putchar(c);
 
-					putchar(c);
-
-					if (c != 57)
+				if (c != 57)
 
 					{
 
-																	putchar(',');
+																putchar(',');
 
-																	putchar(' ');
+																putchar(' ');
 		
-																				}
-			
-									}
+																			}
+		
+								}
 
 			putchar('\n');
 
