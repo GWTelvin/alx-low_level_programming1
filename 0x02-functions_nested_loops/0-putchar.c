233a423ee8e40b4fece7812c3bb355ff7b58f6d8
GWTelvin <telvinwachira75@gmail.com>
2022-09-14 00:51:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,25 +6,22 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
 int main(void)
 {
-	char ch[8] = "_putchar";
-	int i = 0;
-	char c;
-
-	while (i <= 8)
-	{
-	c = ch[i];
-	_putchar(c);
-	i++;
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return(0);
 }
 
