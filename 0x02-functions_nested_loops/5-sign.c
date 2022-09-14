31acfa2560a26cbf33c8066427ecae3b507859c6
GWTelvin <telvinwachira75@gmail.com>
2022-09-14 04:04:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/**print_sign-function to check for a sign of a number 
- * @n: is the int htat will use for the argument of the function
+/**
+ * print_sign-function to check for a sign of a number
+ * @n: is the int that will use for the argument of the function
  * Return: 0
  */
 int print_sign(int n)
@@ -15,8 +16,8 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		return (1);
-}
-	else 
+	}
+	else
 	{
 		_putchar('0');
 		return (0);
