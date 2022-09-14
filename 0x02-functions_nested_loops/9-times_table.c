0edb9ab7bd2e0040f7703e3500418033104dcc1a
GWTelvin <telvinwachira75@gmail.com>
2022-09-14 05:02:50 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -19,8 +19,8 @@ void times_table(void)
 
 			if (prod <= 9)
 				_putchar(',');
-			else 
-				_putchar((prod / 10) + '0');
+			else
+			_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
