8f0e36da051e57b69e76648db098bb0d7dbfa84e
GWTelvin <telvinwachira75@gmail.com>
2022-10-14 03:09:27 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,6 +12,7 @@ typrdef struct print
 	void (*f)(va_list);
 } print_t;
 
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
