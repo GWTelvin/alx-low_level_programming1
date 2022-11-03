9e0d6a8c02c58b096b49150fef7b882a707855ee
GWTelvin <telvinwachira75@gmail.com>
2022-11-02 23:54:29 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,10 +5,9 @@
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *	function should read and print
- *
+ *		function should read and print.
  * Return: If the function fails or filename is NULL - 0.
- *	 O/w - the actual number of bytes the function can read and print.
+ *	O/w - the actual number of bytes the function can read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -27,13 +26,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
+
 	free(buffer);
-	close(0);
+	close(o);
 
 	return (w);
 }
+
+
+
